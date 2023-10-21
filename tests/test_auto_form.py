f38d972bd7288e879fee5a1463c6534c3cb9e63f
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2023-10-21 12:59:50 +0300

    Update test_auto_form.py
    fix conflicts

--- tests/test_auto_form.py
+++ tests/test_auto_form.py
@@ -9,9 +9,5 @@ def test_fill_form():
     browser.element('#userEmail').click().type('test@test.com')
     browser.element('[for="gender-radio-2"]').click()
     browser.element('#userNumber').click().type('780080080804')
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react - datepicker__year - select').click().element(by.text('2000')).click()
-    browser.element('.react-datepicker__month-select').click().element(by.text('August')).click()
-    browser.element('.react-datepicker__week').click().element(by.text('26')).click()
-    browser.element('.subjects-auto-complete__placeholder css-1wa3eu0-placeholder').click().type("Math")
-    browser.element('.custom-control-label').click()
+
+
