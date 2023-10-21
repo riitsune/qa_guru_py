edffc64b57a0e7ef044747278e0dccb1d55f67e3
Rita Shcherbakova <m.shcherbakova@fssoft.ru>
2023-10-21 12:57:27 +0300

    test for filling the form

--- tests/test_auto_form.py
+++ tests/test_auto_form.py
@@ -1,5 +1,6 @@
 from selene.support.shared import browser
-from selene import be, have, by
+from selene import have, command, by
+import os
 
 def test_fill_form():
     browser.open("https://demoqa.com/automation-practice-form")
@@ -8,8 +9,40 @@ def test_fill_form():
     browser.element('#lastName').click().type('Shch')
     browser.element('#userEmail').click().type('test@test.com')
     browser.element('[for="gender-radio-2"]').click()
-    browser.element('#userNumber').click().type('780080080804')
+    browser.element('#userNumber').click().type('78008008080')
+
     browser.element('#dateOfBirthInput').click()
-    browser.element('.react - datepicker__year - select').click().element(by.text('2000'))
-    browser.element('.react-datepicker__month-select').click().element(by.text('August'))
-    browser.element('.react-datepicker__week').click().element(by.text('26'))
+    browser.element('.react-datepicker__year-select').click()
+    browser.element(by.text('2000')).click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element(by.text('August')).click()
+    browser.element(by.text('26')).click()
+
+
+    browser.element('#subjectsInput').click().type('Math').press_enter()
+    # browser.with_(timeout=browser.config.timeout * 3).element('[for="hobbies-checkbox-2"]').click()
+    browser.element('#state').perform(command.js.scroll_into_view)
+    browser.element('#uploadPicture').send_keys(os.path.abspath('image/image.jpg'))
+    browser.element('#currentAddress').click().type("Saints-Petersburg, Sizova av.,9")
+    browser.element('#state').click()
+    browser.element(by.text('Uttar Pradesh')).click()
+    browser.element('#city').click()
+    browser.element(by.text('Agra')).click()
+
+    browser.element('#submit').execute_script('element.click()')
+
+    browser.element('.table').all('tr td:nth-child(2)').should(have.texts
+        (
+        'Rita Shch',
+        'test@test.com',
+        'Female',
+        '7800800808',
+        '26 August,2000',
+        'Maths',
+        '',
+        'image.jpg',
+        'Saints-Petersburg, Sizova av.,9',
+        'Uttar Pradesh Agra'
+    ))
+
+
