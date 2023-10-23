54a25ffc0ea302f70aa21382bfab20e75a5e1e86
Rita Shcherbakova <105156033+riitsune@users.noreply.github.com>
2023-10-23 15:59:49 +0300

    add base_url

--- conftest.py
+++ conftest.py
@@ -3,6 +3,7 @@ from selene.support.shared import browser
 
 @pytest.fixture(scope='function', autouse=True)
 def browser_conf():
+    browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
 
