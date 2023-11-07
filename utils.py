c1c4d50b469e09463886844fcedfe1cd35f5008d
Rita Shcherbakova <m.shcherbakova@fssoft.ru>
2023-11-07 17:17:08 +0300

    hw 7

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import os
 PROJECT_ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 RESOURCES_PATH = os.path.join(PROJECT_ROOT_PATH, 'resources')
 TMP_PATH = os.path.join(PROJECT_ROOT_PATH, 'tmp')
-ARCHIVE_PATH = os.path.join(TMP_PATH, 'new_archive.zip')
+# ARCHIVE_PATH = os.path.join(TMP_PATH, 'new_archive.zip')
 
 TXT_FILE = 'file_txt.txt'
 PDF_FILE = 'Python Testing with Pytest (Brian Okken).pdf'
